727a3d868abbca3e543813a82d0d6ad6ff6a543f
Barry Nguyen <barry.tuan.nguyen@gmail.com>
2021-06-19 14:31:17 +0000

    reset password token mailer added

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -3,11 +3,11 @@ class PasswordResetsController < ApplicationController
     end 
     
     def create
-        @user = User.find.by(email: params[:email])
+        @user = User.find_by(email: params[:email])
         
         if @user.present?
             # send email
-            PasswordMailer.with(user: @user).reset.deliver_later #with - sets params inside mailer 
+            PasswordMailer.with(user: @user).reset.deliver_now #with - sets params inside mailer 
         end 
             redirect_to root_path, notice: "If an account with that email was found, we have sent a link to reset your password"
     end 
